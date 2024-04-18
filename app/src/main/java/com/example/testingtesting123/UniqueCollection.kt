36e34880a686628e24a24b3ab6e274fc11ddb8e2
Eric Lin <tur99299@temple.edu>
2024-04-18 12:23:00 -0400

    test the get item and remove specific item

--- app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
+++ app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
@@ -8,7 +8,8 @@ class UniqueCollection {
             collection.add(item)
     }
 
-    fun get (position: Int) = collection[position]
+    fun get(position: Int) = collection[position]
+
 
     fun remove (item: Item){
         !collection.remove(item)
--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -29,6 +29,24 @@ class UniqueCollectionTest {
 
         assert (collection.size() == 2)
     }
+    @Test
+    fun getItem() {
+        val originalItem = Item("Item1")
+        collection.addItem(originalItem)
+        val item = collection.get(0)
+        assert(originalItem == item) { "Item fetched from collection does not match original item. Expected: ${originalItem.name}, Actual: ${item.name}" }
+    }
+
+    @Test
+    fun removeSpecificItem(){
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+
+        collection.remove(Item("item1"))
+        assertEquals(1, collection.size())
+        //assertEquals("item2", collection.get(0).name)
+
+    }
 
     @Test
     fun clearAllItems() {
